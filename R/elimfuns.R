200a99e9a8f20e2380ac37c3e07fdc1d8decfd40
Cesko Voeten <c.c.voeten@hum.leidenuniv.nl>
2017-12-18 10:16:49 +0100

    Avoid unreliable df.residual() and suppress double p-value message

--- R/elimfuns.R
+++ R/elimfuns.R
@@ -11,14 +11,12 @@ modcomp.LRT <- function (p) {
 			n <- length(m$y)
 			dfc <- if (is.null(m$edf2)) 0 else sum(m$edf2) - sum(m$edf)
 			n - sum(m$edf1) - dfc
-		} else df.residual(m)
+		} else attr(logLik(m),'df')
 	}
 
 	chLL <- -2*(logLik(p$mb) - logLik(p$ma))
-	chdf <- getdf(p$mb) - getdf(p$ma)
+	chdf <- getdf(p$ma) - getdf(p$mb)
 	pval <- pchisq(chLL,chdf,lower.tail=F)
-	pval <- if (p$reml) pval/2 else pval # Wood (2017) shows that LRTing of random effects is not completely valid, Pinheiro & Bates (2000) suggest that the appropriate correction is to divide the p-value by 2.
+	if (p$reml) pval/2 else pval # Wood (2017) shows that LRTing of random effects is not completely valid, Pinheiro & Bates (2000) suggest that the appropriate correction is to divide the p-value by 2.
 	# I do not follow Baayen (2008) in dividing by 2 also when testing fixed effects, because the chi-square test should in principle be the asymptotic equivalent of an F test; without division, the X^2 p-value closely matches the F p-value, but with division, it obviously won't.
-	if (!p$quiet) message(paste('Significance of change in log-likelihood:',pval))
-	pval
 }
