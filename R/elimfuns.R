96d1794c9a35cf73fa55bc52864330a338b4f93a
Cesko Voeten <c.c.voeten@hum.leidenuniv.nl>
2018-02-10 21:31:00 +0100

    Ensure that df are >0 when comparing models

--- R/elimfuns.R
+++ R/elimfuns.R
@@ -16,6 +16,7 @@ modcomp.LRT <- function (p) {
 
 	chLL <- -2*(logLik(p$mb) - logLik(p$ma))
 	chdf <- getdf(p$ma) - getdf(p$mb)
+	if (chdf <= 0) return(1)
 	pval <- pchisq(chLL,chdf,lower.tail=F)
 	if (p$reml) pval/2 else pval # Wood (2017) shows that LRTing of random effects is not completely valid, Pinheiro & Bates (2000) suggest that the appropriate correction is to divide the p-value by 2.
 	# I do not follow Baayen (2008) in dividing by 2 also when testing fixed effects, because the chi-square test should in principle be the asymptotic equivalent of an F test; without division, the X^2 p-value closely matches the F p-value, but with division, it obviously won't.
