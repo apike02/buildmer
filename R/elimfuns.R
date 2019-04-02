b2e14a1cdcb2c3d4b13a4fff6d8577f8a66bed65
Cesko Voeten <c.c.voeten@hum.leidenuniv.nl>
2019-04-02 10:24:34 +0200

    Add ordering (and elimination) criterion based on raw log-likelihood and fix error in Julia log-likelihood calculation

--- R/elimfuns.R
+++ R/elimfuns.R
@@ -1,12 +1,17 @@
-elfun.AIC <- function (AICdiff) AICdiff > -.001
-elfun.BIC <- function (BICdiff) BICdiff > -.001
+get2LL <- function (m) as.numeric(-2*stats::logLik(m))
+getdf  <- function (m) attr(stats::logLik(m),'df')
+get2LL.julia <- function (julia,m) if (inherits(m,'JuliaObject')) -2*julia$call('loglikelihood',m) else get2LL(m)
+getdf.julia  <- function (julia,m) if (inherits(m,'JuliaObject'))    julia$call('dof',m)           else getdf(m)
+
+elfun.AIC <- function (diff) diff > -.001
+elfun.BIC <- function (diff) diff > -.001
 elfun.LRT <- function (logp) exp(logp) >= .05
+elfun.2LL <- function (diff) diff > -.001
+elfun.LL <- elfun.2LL
 
 crit.AIC <- function (ref,alt) if (is.null(ref)) stats::AIC(alt) else stats::AIC(alt) - stats::AIC(ref)
 crit.BIC <- function (ref,alt) if (is.null(ref)) stats::BIC(alt) else stats::BIC(alt) - stats::BIC(ref)
 crit.LRT <- function (ref,alt) {
-	get2LL <- function (m) as.numeric(-2*stats::logLik(m))
-	getdf  <- function (m) attr(stats::logLik(m),'df')
 	if (is.null(ref)) {
 		chLL <- get2LL(alt)
 		chdf <- getdf(alt)
@@ -17,14 +22,14 @@ crit.LRT <- function (ref,alt) {
 	if (chdf <= 0) return(0)
 	stats::pchisq(chLL,chdf,lower.tail=F,log.p=T)
 }
+crit.2LL <- function (ref,alt) if (is.null(ref)) get2LL(alt) else get2LL(alt) - get2LL(ref)
+crit.LL <- crit.2LL
 
 AIC.julia <- function (julia,m) if (inherits(m,'JuliaObject')) julia$call('StatsBase.aic',m) else stats::AIC(m)
 BIC.julia <- function (julia,m) if (inherits(m,'JuliaObject')) julia$call('StatsBase.bic',m) else stats::BIC(m)
 crit.AIC.julia <- function (julia,ref,alt) if (is.null(ref)) AIC.julia(julia,alt) else AIC.julia(julia,alt) - AIC.julia(julia,ref)
 crit.BIC.julia <- function (julia,ref,alt) if (is.null(ref)) BIC.julia(julia,alt) else BIC.julia(julia,alt) - BIC.julia(julia,ref)
 crit.LRT.julia <- function (julia,ref,alt) {
-	get2LL <- function (m) if (inherits(m,'JuliaObject')) julia$call('loglikelihood',m) else as.numeric(stats::logLik(m))
-	getdf  <- function (m) if (inherits(m,'JuliaObject')) julia$call('dof',m) else attr(stats::logLik(m),'df')
 	if (is.null(ref)) {
 		chLL <- get2LL(alt)
 		chdf <- getdf(alt)
@@ -35,3 +40,5 @@ crit.LRT.julia <- function (julia,ref,alt) {
 	if (chdf <= 0) return(0)
 	stats::pchisq(chLL,chdf,lower.tail=F,log.p=T)
 }
+crit.2LL.julia <- function (julia,ref,alt) if (is.null(ref)) get2LL.julia(julia,alt) else get2LL.julia(julia,alt) - get2LL.julia(julia,ref)
+crit.LL.julia <- crit.2LL.julia
