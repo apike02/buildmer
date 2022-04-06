b06da6f6b51f420df34efda69d47c2a2b4fda046
Cesko Voeten <cvoeten@gmail.com>
2022-04-06 19:41:04 +0200

    Test updates for GLMMadaptive

--- tests/testthat/test-buildGLMMadaptive.R
+++ tests/testthat/test-buildGLMMadaptive.R
@@ -6,3 +6,9 @@ test_that('buildGLMMadaptive',{
 	       family=binomial,data=vowels,buildmerControl=list(args=list(nAGQ=1)))
 	buildmer:::testthat.compare.df(model@p$results,'buildGLMMadaptive')
 })
+test_that('buildGLMMadaptive-diag',{
+	skip_on_cran()
+	model <- buildGLMMadaptive(stress ~ vowel + (vowel||word),
+	       family=binomial,data=vowels,buildmerControl=list(args=list(nAGQ=1)))
+	buildmer:::testthat.compare.df(model@p$results,'buildGLMMadaptive-diag')
+})
